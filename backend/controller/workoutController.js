1505bf87dc08621ae45a878ed1ff652dffa6105a
Jenna <jenna48071@gmail.com>
2024-04-08 16:56:58 -0500

    assigning workouts to users

--- backend/controller/workoutController.js
+++ backend/controller/workoutController.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 
 // all workouts - /api/workouts
 const displayWorkouts = async (req, res) => {
-  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  const user_id = req.user._id;
+  const workouts = await Workout.find({ user_id }).sort({ createdAt: -1 });
   res.status(200).json(workouts);
 };
 
@@ -30,7 +31,8 @@ const createWorkout = async (req, res) => {
   }
 
   try {
-    const workout = await Workout.create({ title, load, reps });
+    const user_id = req.user_id;
+    const workout = await Workout.create({ title, load, reps, user_id });
     res.status(201).json(workout);
   } catch (error) {
     res.status(400).json({ error: error.message });
--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,13 +1,19 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutContext.js";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: workoutsDispatch } = useWorkoutsContext();
 
   const logout = () => {
     // Remove user from local storage
     localStorage.removeItem("user");
+
     // Dispatch logout
     dispatch({ type: "LOGOUT" });
+
+    // Clear global workout state
+    workoutsDispatch({ type: "SET_WORKOUTS", payload: null });
   };
 
   return { logout };
